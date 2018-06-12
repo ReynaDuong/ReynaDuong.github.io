aa55d78783491603fb7fec595a9ed638c46124a1
ReynaDuong <minhduong.md25@gmail.com>
2018-06-12 15:01:34 +0700

    skeleton

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include("header.html");
+    
+    if (isset($_POST['submit'])) {
+        header('Location: aboutme.html');
+    }
+?>
+
+<div class="menu">
+
+</div>
+
+<div class="container">
+    <h1>HI, MY NAME IS MINH DUONG</h1>
+    <p>I am an energetic and passionate college student working toward a B.S. in Software Engineering</p>
+
+    
+</div>
+
+
+<!--    <form method="post">-->
+<!--        <input type="submit" name="submit" value="About Me"/>-->
+<!--    </form>-->
